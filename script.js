f97cbe3a28a016f5e5432d92ebeff4eab80a61ad
Devarshi Shimpi <devarshishimpi@gmail.com>
2022-08-12 15:56:14 +0530

    add navbar

--- script.js
+++ script.js
@@ -98,3 +98,21 @@ addBtn.addEventListener("click", e => {
         closeIcon.click();
     }
 });
+
+//navbar
+
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
