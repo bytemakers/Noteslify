452aef1888402262a76f7fe76ac6a90b906682d1
Devarshi Shimpi <devarshishimpi@gmail.com>
2022-08-28 23:31:47 +0530

    add forgot password

--- frontend/src/components/ForgotPassword/ForgotPassword.js
+++ frontend/src/components/ForgotPassword/ForgotPassword.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import styles from './ForgotPassword.css';
+import './ForgotPassword.css';
 
 const ForgotPassword = () => (
   <div>
-    ForgotPassword Component
+    <div class="form-wrapper">
+      <h1>Forgot Password?</h1>
+      <br></br>
+      <p>Please enter your registered email to receive your forgot Password Link</p>
+      <br></br>
+      <form>
+        <div className="form-item">
+          <label for="email"></label>
+          <input type="email" name="email" required="required" placeholder="Email Address"></input>
+        </div>
+        <div className="button-panel">
+          <input type="submit" class="button" title="Sign In" value="Send Email"></input>
+        </div>
+      </form>
+      <div className="form-footer">
+        <p>Remember Your Password? <a href="/login">Login</a></p>
+      </div>
+    </div>
   </div>
 );
 
