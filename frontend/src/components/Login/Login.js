2c058127c6395afa09eb1b474762c49c643fca17
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-29 16:48:27 +0530

    Making Link to Forgot Password Page

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -114,7 +114,7 @@ const Login = () => {
                         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" className="form__input" id="password"/>
                     </div>
                 </div>
-                <a href="/forgotpassword" className="form__forgot">Forgot Password?</a>
+                <Link to="/forgotpassword" className="form__forgot">Forgot Password?</Link>
 
                 {!isLoading && <button type="submit" className="form__button">Login</button>}
                 {isLoading && <button type="submit" className="form__button" style={{backgroundColor: '#15203a', cursor: 'not-allowed'}} disabled={true}><span class="loader"></span></button>}
