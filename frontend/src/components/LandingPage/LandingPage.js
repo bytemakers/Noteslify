c45d74a8efab27427191d1123c579b310f0d30a2
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-30 13:27:41 +0530

    Adding the Landing Page

---
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import About from './components/About'
+import Info from './components/Info'
+import Navbar from './components/Navbar'
+import './LandingPage.css'
+
+const LandingPage = () => {
+  return (
+    <div className="font-['Poppins'] font-[300] overflow-y-auto overflow-x-hidden">
+        <Navbar/>
+        <About/>
+        <Info/>
+    </div>
+  )
+}
+
+export default LandingPage
