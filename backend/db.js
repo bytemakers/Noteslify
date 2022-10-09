8761baa3f58072900ded1e196da452511d185523
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2022-10-09 20:46:29 +1300

    Ensure Mongo is connected first before everything

--- backend/db.js
+++ backend/db.js
@@ -17,14 +17,27 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017";
 
-const connectToMongo = () => {
-    mongoose.connect(mongoURI, { dbName: 'noteslify' }, (error) => {
-        if (error) {
-            console.error(error)
-        }
+const connectToMongo = async (retryCount) => {
+    const MAX_RETRIES = 3;
+    const count = retryCount ?? 0;
+
+    try {
+        await mongoose.connect(mongoURI, { dbName: 'noteslify' });
+        console.info('Connected To Mongo Successfully!!');
+
+        return;
+    } catch (error) {
+        console.error(error);
+
+        const nextRetryCount = count + 1;
         
-        console.log("Connected To Mongo Successfully!!");
-    })
+        if (nextRetryCount >= MAX_RETRIES)
+            throw new Error("Unable to connect to Mongo");
+
+        console.info(`Retrying, retry count: ${nextRetryCount}`);
+        
+        return await connectToMongo(nextRetryCount)
+    }
 };
 
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,26 @@ const express = require('express');
 const connectToMongo = require('./db');
 const cors = require('cors');
 
-connectToMongo();
+const load = async () => {
+    const app = express();
+    const PORT = process.env.PORT || 8181;
+    
+    app.use(express.json());
+    app.use(cors());
+    
+    // Available Routes
+    app.use('/api/auth', require('./routes/auth'));
+    app.use('/api/notes', require('./routes/notes'));
+    
+    app.get('/', (req, res) => {
+        res.send('Hi!');
+    });
+    
+    app.listen(PORT, () => {
+        console.log(`The App is running at http://localhost:${PORT}`);
+    });
 
-const app = express();
-const PORT = process.env.PORT || 8181;
+    await connectToMongo();
+}
 
-app.use(express.json());
-app.use(cors());
-
-// Available Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
-
-app.get('/', (req, res) => {
-    res.send('Hi!');
-});
-
-app.listen(PORT, () => {
-    console.log(`The App is running at http://localhost:${PORT}`);
-});
+load()
