86c177b408b16ee3d326abbbf5538b784efaa809
Pcamellon <pedropcamellon@gmail.com>
2022-10-12 09:16:28 -0400

    Add custom Newsletter section

--- frontend/src/components/LandingPage/Pages/Home.js
+++ frontend/src/components/LandingPage/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from '../Components/NavBar/NavBar'
 import HeroSection from '../Components/HeroSection/HeroSection'
-import Info from'../Components/Info/Info'
+import Info from '../Components/Info/Info'
 import Footer from '../Components/Footer/Footer'
 import NewsLetter from '../Components/Newsletter/NewsLetter'
 import About from '../Components/About/About'
@@ -10,12 +10,12 @@ import '../LandingPage.css'
 const Home = () => {
   return (
     <>
-        <NavBar/>
-        <HeroSection/>
-        <About/>
-        <Info/>
-        <NewsLetter/>
-        <Footer/>
+      <NavBar />
+      <HeroSection />
+      <About />
+      <Info />
+      {/* <NewsLetter/> */}
+      <Footer />
     </>
   )
 }
