0efc0f82852da127792c305bf10e015ca7fb7fc2
Pcamellon <pedropcamellon@gmail.com>
2022-10-12 09:21:57 -0400

    Add custom Newsletter section.

--- frontend/src/components/LandingPage/Components/Newsletter/NewsLetter.js
+++ frontend/src/components/LandingPage/Components/Newsletter/NewsLetter.js
@@ -3,39 +3,16 @@ import "./NewsLetter.css";
 
 const NewsLetter = () => {
   return (
-    <section className="newsletter-section flex p-4 bg-primary">
-      <form action="" class="newsletter__form text-center">
-        <h2>Subscribe to Our Newsletter</h2>
-        <h4 class="mb-1">You won’t get dissapointed by us</h4>
-        <label for="mail"></label>
-        <div class="newsletter__form__email flex">
-          <input
-            type="text"
-            placeholder="Your Email"
-            name="mail"
-            required
-            class="newsletter__form__email__your_email"
-          />
-          <input
-            type="submit"
-            value="Subscribe"
-            class="newsletter__form__email__submit_btn bg-primary"
-          />
-        </div>
-        <div class="newsletter__form__email_copy flex mt-1">
-          <label class="newsletter__form__email_copy__label">
-            <input
-              type="checkbox"
-              name="checkbox"
-              class="newsletter__form__email_copy__check_box"
-            />
-          </label>
-
-          <span class="text-left">Email me a copy of my responses</span>
-        </div>
-      </form>
+    <section className="news-section">
+      <iframe
+        title="newsletter"
+        src="https://airtable.com/embed/shrUwGnyLnDvLvBoJ?backgroundColor=purple"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        allowFullScreen=""
+        style={{ position: "absolute", top: "0", left: "0" }}></iframe>
     </section>
   );
 };
-
 export default NewsLetter;
