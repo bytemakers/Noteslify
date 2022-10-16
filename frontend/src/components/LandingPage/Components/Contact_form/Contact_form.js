551d483343ef3ee768a93bf8f85ac693f9430bfe
Devarshi Shimpi <devarshishimpi@gmail.com>
2022-10-16 18:30:08 +0530

    made contact form working

--- frontend/src/components/LandingPage/Components/Contact_form/Contact_form.js
+++ frontend/src/components/LandingPage/Components/Contact_form/Contact_form.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Contact_form.css'
 import contactImg from './Contact.png'
+import { useForm, ValidationError } from '@formspree/react';
 
 const Contact_form = () => {
+  const [state, handleSubmit] = useForm("mrgdlnqk");
+  if (state.succeeded) {
+      return <p>Thanks for Contacting!! We'll be back to you soon.</p>;
+  }
   return (
     <section className='contact-section Container flex'>
         <div className = "img-sec">
@@ -11,18 +16,33 @@ const Contact_form = () => {
     <div >
        <div className="Contact-form">
            <h2>Contact Us</h2>
-           <form>
+           <form onSubmit={handleSubmit}>
                <div>
                <input type="text" name = "name" placeholder ="Name"required/>
+               <ValidationError 
+                  prefix="Name" 
+                  field="name"
+                  errors={state.errors}
+                />
             </div>
             
              <div>
                 <input type="email" name = "email" placeholder ="Email" required/>
              </div>
+                  <ValidationError 
+                    prefix="Email" 
+                    field="email"
+                    errors={state.errors}
+                  />
              <div>
-                <textarea id = "qA-textBox" row = "30" cols = "50"></textarea>
+                <textarea id="message" name="message" placeholder ="Message" row = "30" cols = "50"></textarea>
+                <ValidationError 
+                  prefix="Message" 
+                  field="message"
+                  errors={state.errors}
+                />
              </div>
-             <button>Send</button>
+             <button type="submit" disabled={state.submitting}>Send</button>
            </form>
        </div>
     </div>
