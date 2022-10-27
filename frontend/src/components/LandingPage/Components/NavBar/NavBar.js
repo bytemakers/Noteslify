2980b04dfe2d939f89ae078a49a98f4b3c7abd3b
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2022-10-27 13:49:55 +0530

    Added hamburger to close animation and blured dropdown bg

--- frontend/src/components/LandingPage/Components/NavBar/NavBar.js
+++ frontend/src/components/LandingPage/Components/NavBar/NavBar.js
@@ -16,10 +16,11 @@ const NavBar = () => {
             <h3 className="logo-text">Noteslify</h3>
           </Link>
         </span>
-        <div className="hamburger" onClick={onClickHandler}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-          </svg>
+        <div
+          className={`${"hamburger"} ${navbar && "open"}`}
+          onClick={onClickHandler}
+        >
+          <div class={`${"btn__burger"} `}></div>
         </div>
         <ul className={`elements ${!navbar && "show"} ${navbar && "hide"}`}>
           <li className="list">
