63348b75e61e6463c6701713e66a10dec225ac0a
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2022-10-29 14:31:06 +1300

    NavigationProvider inside NativeBaseProvider since screens depend on NativeBaseProvider

--- apps/mobile/noteslify/App.js
+++ apps/mobile/noteslify/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+
 import { NativeBaseProvider } from "./context/native-base";
 import { NavigationProvider } from "./context/navigation";
 
 const App = ({ children }) => (
-	<NavigationProvider>
-		<NativeBaseProvider>{children}</NativeBaseProvider>
-	</NavigationProvider>
+	<NativeBaseProvider>
+		<NavigationProvider>{children}</NavigationProvider>
+	</NativeBaseProvider>
 );
 
 export default App;
