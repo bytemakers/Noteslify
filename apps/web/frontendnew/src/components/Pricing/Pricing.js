4c6cbbcfb62148ac11321dfeb131b61812353e57
Devarshi Shimpi <devarshishimpi@gmail.com>
2022-11-22 15:40:44 +0000

    added testimonials and fixed routes

--- apps/web/frontendnew/src/components/Pricing/Pricing.js
+++ apps/web/frontendnew/src/components/Pricing/Pricing.js
@@ -8,7 +8,7 @@ const Pricing = () => {
 
     const navigation = [
         { name: 'Home', href: '/' },
-        { name: 'Features', href: '#features' },
+        { name: 'Features', href: '/#features' },
         { name: 'Pricing', href: '/pricing' },
         { name: 'Contact', href: '/contact' },
         { name: 'Dashboard', href: 'https://noteslifysystem.stonecss.com/Notes' },
