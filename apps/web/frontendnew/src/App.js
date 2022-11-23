6ef149b512a27e225d754952a81f5f93ede87974
Devarshi Shimpi <devarshishimpi@gmail.com>
2022-11-23 15:28:16 +0000

    added contact page

--- apps/web/frontendnew/src/App.js
+++ apps/web/frontendnew/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home';
 import Pricing from './components/Pricing/Pricing';
+import Contact from './components/Contact/Contact';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Pricing" element={<Pricing />} />
+        <Route path="/Contact" element={<Pricing />} />
       </Routes>
     </Router>
   );
