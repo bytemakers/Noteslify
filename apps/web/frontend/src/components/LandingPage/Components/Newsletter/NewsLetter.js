5ab433cf1d8f5938a9f8026a90b4bffda366c99a
Zenith Anthony <dabarerawza.19@uom.lk>
2023-06-07 16:07:46 +0530

    issue #284  - Subscribe button not centered properly fixed.
    Add me as a cotributor

--- apps/web/frontend/src/components/LandingPage/Components/Newsletter/NewsLetter.js
+++ apps/web/frontend/src/components/LandingPage/Components/Newsletter/NewsLetter.js
@@ -8,32 +8,32 @@ const NewsLetter = () => {
         return <p>Thanks for joining! You'll be updated!</p>;
     }
     return (
-        <section className="newsletter-section flex p-4 bg-primary">
-            <form onSubmit={handleSubmit} class="newsletter__form text-center">
-                <h2>Subscribe to Our Newsletter</h2>
-                <h4 class="mb-1">You won’t get dissapointed by us</h4>
-                <label for="mail"></label>
-                <div class="newsletter__form__email flex">
-                    <input
-                        type="email"
-                        placeholder="Your Email"
-                        name="mail"
-                        required
-                        class="newsletter__form__email__your_email"
-                    />
-                    <ValidationError 
-                        prefix="Email" 
-                        field="email"
-                        errors={state.errors}
-                    />
-                    <input
-                        type="submit"
-                        value="Subscribe"
-                        class="newsletter__form__email__submit_btn bg-primary"
-                        disabled={state.submitting}
-                    />
-                </div>
-                {/*
+      <section className="newsletter-section flex p-4 bg-primary">
+        <form onSubmit={handleSubmit} class="newsletter__form text-center">
+          <h2>Subscribe to Our Newsletter</h2>
+          <h4 class="mb-1">You won’t get dissapointed by us</h4>
+
+          <div class="newsletter__form__email flex">
+            <input
+              type="email"
+              placeholder="Your Email"
+              name="mail"
+              required
+              class="newsletter__form__email__your_email"
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <input
+              type="submit"
+              value="Subscribe"
+              class="newsletter__form__email__submit_btn bg-primary"
+              disabled={state.submitting}
+            />
+          </div>
+          {/*
                 <div class="newsletter__form__email_copy flex mt-1">
                     <label class="newsletter__form__email_copy__label">
                         <input
@@ -46,8 +46,8 @@ const NewsLetter = () => {
                     <span class="text-left">Email me a copy of my responses</span>
                 </div>
                 */}
-            </form>
-        </section>
+        </form>
+      </section>
     );
 };
 
