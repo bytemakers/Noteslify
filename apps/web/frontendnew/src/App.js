b3556ff80b5752f8a615a6fb78f4e1c8fb53583e
Toni Isopoussu <112643869+Pultti@users.noreply.github.com>
2024-02-12 23:01:21 +0200

    commenting App.js
    Basic commenting added for newbies in React that might find your project interesting and wants to learn.

--- apps/web/frontendnew/src/App.js
+++ apps/web/frontendnew/src/App.js
@@ -1,14 +1,21 @@
+// Importing necessary components and hooks from react-router-dom for navigation and routing
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Importing individual page components to be used in routing
 import Home from "./pages/Home/Home";
 import Pricing from "./pages/Pricing/Pricing";
 import Contact from "./pages/Contact/Contact";
-import TopNav from "./components/Navbar/TopNav";
 import Notes from "./pages/Notes/Notes";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/Login/SignUp";
 
+// Importing the TopNav component which will be displayed across all pages
+import TopNav from "./components/Navbar/TopNav";
+
+// Defining the main App component
 function App() {
   return (
+    // Utilizing the BrowserRouter (alias Router) for enabling navigation between components
     <Router>
       {/* top nav */}
       <TopNav />
@@ -24,4 +31,5 @@ function App() {
   );
 }
 
+// Exporting the App component for use in the index.js file
 export default App;
